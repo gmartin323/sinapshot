0d03e56f877f6177255b3ec5e602b9eba9fbf61a
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2023-10-25 18:58:59 +0700

    updated header layout

--- src/components/HorizontalScrollCarousel.jsx
+++ src/components/HorizontalScrollCarousel.jsx
@@ -8,21 +8,21 @@ export default function HorizontalScrollCarousel() {
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-75%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-62.7%"]);
 
   return (
     <section ref={targetRef} className="photo-section" >
       <div className="photos-container">
         <motion.div style={{ x }} className="card-container">
-          <div className="photo">1</div>
-          <div className="photo">2</div>
-          <div className="photo">3</div>
-          <div className="photo">4</div>
-          <div className="photo">5</div>
-          <div className="photo">6</div>
-          <div className="photo">7</div>
-          <div className="photo">8</div>
-          <div className="photo">9</div>
+          <div className="photo"></div>
+          <div className="photo"></div>
+          <div className="photo"></div>
+          <div className="photo"></div>
+          <div className="photo"></div>
+          <div className="photo"></div>
+          <div className="photo"></div>
+          <div className="photo"></div>
+          <div className="photo"></div>
         </motion.div>
       </div>
     </section>
